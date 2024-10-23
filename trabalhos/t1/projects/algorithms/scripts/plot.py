733cf3f8371298943dd4ef88bad14f3522ff869e
lgtozevich <lgtozevich@inf.ufsm.br>
2024-10-23 10:39:34 -0300

    refactor(trabalhos/t1): optimize data reading and plotting in plot.py

--- trabalhos/t1/projects/algorithms/scripts/plot.py
+++ trabalhos/t1/projects/algorithms/scripts/plot.py
@@ -9,44 +9,56 @@ algorithm_labels = {
     'quicksort': 'Quicksort',
     'quicksert': 'Quicksert',
     'insertionsort': 'Insertionsort',
+    'iterative_mergesort': 'Mergesort Iterativo',
 }
 
-def parse_data(file_path):
-    sizes = []
-    times = []
-    algorithm = ''
-    source = ''
-    
+def read_data_from_file(file_path):
+    sizes = {}
+    algorithm = ""
+    source = ""
+
     with open(file_path, 'r') as file:
         lines = file.readlines()
         
-        algorithm = lines[0].strip().split(': ')[1]
-        source = lines[1].strip().split(': ')[1]
+        algorithm = lines[0].strip().split(": ")[1]
+
+        if algorithm in algorithm_labels:
+            algorithm = algorithm_labels[algorithm]
+
+        source = lines[1].strip().split(": ")[1]
 
         for line in lines[2:]:
-            size, time = line.strip().split(': ')
-            sizes.append(int(size))
-            times.append(float(time))
+            parts = line.strip().split(": ")
+            if len(parts) == 2:  
+                size = int(parts[0])
+                times = list(map(float, parts[1].split()))
+                if size not in sizes:
+                    sizes[size] = []
+                sizes[size].extend(times)
+                print(sizes[size])
     
-    return algorithm, source, sizes, times
+    avg_times = {size: sum(times) / len(times) for size, times in sizes.items()}
+
+    return algorithm, source, list(avg_times.keys()), list(avg_times.values())
+
 
 def plot_data(files):
     plt.figure(figsize=(10, 6))
     algorithms_used = []
 
     for file in files:
-        algorithm, source, sizes, times = parse_data(file)
-        label = f'{algorithm_labels.get(algorithm, algorithm)} - {source}'
-        plt.plot(sizes, times, label=label)
+        algorithm, source, sizes, avg_times = read_data_from_file(file)
+        label = f"{algorithm} - {source}"
+        plt.plot(sizes, avg_times, label=label)
         algorithms_used.append(algorithm)
     
     plt.xlabel('Número de elementos')
-    plt.ylabel('Tempo (ns)')
+    plt.ylabel('Tempo médio (ns)')
     
     plt.legend()
     plt.grid(True)
     
-    file_name = 'x'.join(algorithms_used).replace(' ', '_') + '.png'
+    file_name = 'x'.join(algorithms_used).replace(" ", "_") + '.png'
     
     plt.savefig(file_name)
     plt.show()
