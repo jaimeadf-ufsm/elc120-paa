ba7a07d6092bf11fa66edde30df2ba039f5fa09e
Jaime Daniel <jaimeadanielfilho@hotmail.com>
2024-10-24 10:38:47 -0300

    feat(trabalhos/t1): comment merge sort algorithm

--- trabalhos/t1/projects/algorithms/src/mergesort.c
+++ trabalhos/t1/projects/algorithms/src/mergesort.c
@@ -13,55 +13,90 @@ struct mergesort_thread_args
     int right;
 };
 
-// define um limite para determinar quando a ordenação paralela deve ser interrompida
+// Define um limiar para determinar quando a ordenação paralela deve ser
+// interrompida.
 #define MERGESORT_PARALLEL_THRESHOLD 100000
 
-// mescla 2 subarrays em um array de destino (ordenado)
+// Junta os subvetores ordenados em source[left..mid] e em source[mid+1..right]
+// para destination[left..right].
 void merge(int source[], int destination[], int left, int mid, int right)
 {
     int i = left;
     int j = mid + 1;
 
+    int k = left;
+
+    // Percorre os subvetores esquerdo e direito simultaneamente,
+    // mesclando os elementos em ordem crescente.
     while (i <= mid && j <= right)
     {
         if (source[i] < source[j])
         {
-            destination[left++] = source[i++]; // adiciona elemento do subarray esquerdo
+            // Copia o elemento do subvetor esquerdo,
+            // quando ele for menor que o do subvetor direito.
+            destination[k++] = source[i++];
         }
         else
         {
-            destination[left++] = source[j++]; // Adiciona elemento do subarray direito
+            // Copia o elemento do subvetor direito,
+            // quando ele for menor que o do subvetor esquerdo ou igual.
+            destination[k++] = source[j++];
         }
     }
 
-    // copia os elementos restantes do subarray esquerdo, se houver
+    // Copia os elementos restantes do subvetor esquerdo, se houver.
     while (i <= mid)
     {
-        destination[left++] = source[i++];
+        destination[k++] = source[i++];
     }
 
-    // copia os elementos restantes do subarray direito, se houver
+    // Copia os elementos restantes do subvetor direito, se houver.
     while (j <= right)
     {
-        destination[left++] = source[j++];
+        destination[k++] = source[j++];
     }
 }
 
+// Aplica o Merge Sort recursivamente no subvetor source[left..right] e
+// armazena o resultado em destination[left..right].
+//
+// Isso é realizado em duas etapas:
+// 1. Divisão: divide o vetor em dois subvetores de tamanhos iguais.
+// 2. Conquista: mescla os subvetores, ordenando-os.
+//
+// A cada chamada recursiva, os vetores source e destination são trocados
+// com o objetivo de evitar cópias desnecessárias.
+//
+// OBS: Inicialmente, os vetores soruce e destination devem estar preenchidos
+// com os mesmos valores.
 void mergesort_recursion(int source[], int destination[], int left, int right)
 {
+    // Caso base:
+    // Se o subvetor tiver tamanho 0 ou 1, ele já está ordenado.
     if (left >= right)
     {
         return;
     }
 
+    // Divisão:
+    // Subdivide o vetor em dois subvetores de tamanhos iguais:
+    // - Subvetor esquerdo: destination[left..mid]
+    // - Subvetor direito: destination[mid+1..right]
+    //
+    // Em seguida, aplica o Merge Sort recursivamente em cada subvetor,
+    // escrevendo o resultado em source[left..right].
     int mid = (left + right) / 2;
 
     mergesort_recursion(destination, source, left, mid);
     mergesort_recursion(destination, source, mid + 1, right);
+
+    // Conquista:
+    // Mescla os subvetores ordenados em source[left..mid] e em
+    // source[mid+1..right] para destination[left..right].
     merge(source, destination, left, mid, right);
 }
 
-// realiza o mergesort de forma recursiva
+// Executa o Merge Sort de forma recursiva.
 void recursive_mergesort(int array[], int size)
 {
     int *auxiliary = malloc(size * sizeof(int));
@@ -75,7 +110,7 @@ void recursive_mergesort(int array[], int size)
     free(auxiliary);
 }
 
-// realiza o mergesort de forma iterativa
+// Executa o Merge Sort de forma iterativa.
 void iterative_mergesort(int array[], int size)
 {
     int *auxiliary = malloc(size * sizeof(int));
@@ -83,10 +118,10 @@ void iterative_mergesort(int array[], int size)
     int *source = array;
     int *destination = auxiliary;
 
-    // itera sobre o array, mesclando subarrays de tamanho 1, 2, 4 ...
+    // Itera sobre o vetor, dividindo-o em subvetores de tamanhos crescentes.
     for (int width = 1; width < size; width *= 2)
     {
-        // mescla os subarrays em pares
+        // Mescla os subvetores de tamanho width em pares.
         for (int i = 0; i < size; i += 2 * width)
         {
             int left = i;
@@ -107,14 +142,19 @@ void iterative_mergesort(int array[], int size)
             merge(source, destination, left, mid, right);
         }
 
-        // troca os ponteiros para o proximo iteracao
+        // Troca os vetores source e destination para evitar cópias
+        // desnecessárias.
         int *temp = source;
 
         source = destination;
         destination = temp;
     }
 
-    // se precisar copia o array ordenado para o array original
+    // Caso o vetor original já não seja o vetor de destino, copia o vetor
+    // ordenado para o vetor original.
+    //
+    // Note que o vetor source é o destino da última iteração, visto que a troca
+    // ocorre no final do laço de repetição.
     if (source != array)
     {
         for (int i = 0; i < size; i++)
@@ -126,6 +166,8 @@ void iterative_mergesort(int array[], int size)
     free(auxiliary);
 }
 
+// Aplica o Merge Sort recursivamente, assim como na função de recursão
+// original, criando threads para realizar a ordenação de forma paralela.
 void *parallel_mergesort_recursion(void *args)
 {
     MergesortThreadArgs *mergesort_args = (MergesortThreadArgs *)args;
@@ -143,7 +185,9 @@ void *parallel_mergesort_recursion(void *args)
     int n = right - left + 1;
     int mid = (left + right) / 2;
 
-    if (n > MERGESORT_PARALLEL_THRESHOLD) // se o tamanho do subarray exceder o limite, cria threads para ordenar em paralelo
+    // Se o tamanho do subvetor exceder o limiar predefinido,
+    // cria threads para realizar a ordenação em paralelo.
+    if (n > MERGESORT_PARALLEL_THRESHOLD) 
     {
         pthread_t left_thread, right_thread;
         MergesortThreadArgs left_args = {destination, source, left, mid};
@@ -157,7 +201,7 @@ void *parallel_mergesort_recursion(void *args)
     }
     else
     {
-        // caso contrário, realiza a ordenação de forma recursiva
+        // Caso contrário, realiza a ordenação de forma recursiva.
         mergesort_recursion(destination, source, left, mid);
         mergesort_recursion(destination, source, mid + 1, right);
     }
@@ -167,7 +211,7 @@ void *parallel_mergesort_recursion(void *args)
     return NULL;
 }
 
-// realiza o mergesort de forma paralela
+// Executa o Merge Sort de forma paralela.
 void parallel_mergesort(int array[], int size)
 {
     int *auxiliary = malloc(size * sizeof(int));
